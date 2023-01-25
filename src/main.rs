3fe2f5bdbe217baec2497f6acaea8a6824f8d1f2
pablo <pablo@quadrosoluciones.mx>
2023-01-24 20:22:59 -0800

    nothing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,34 @@
 use std::io;
 fn main() {
+    operations();
+}
+fn operations() {
+    let num_1: f32 = 1.111111111;
+    let num_2: f32 = 0.33333333333333333333333333333;
+    println!("Sum: {}", num_1 + num_2);
+    let num_3: u32 = 17;
+    let num_4: u32 = 68;
+    println!("{num_3} + {num_4} = {}", num_3 + num_4);
+    println!("{num_3} - {num_4} = {}", num_3 * num_4);
+    // println!("{num_3} * {num_4} = {}", num_3 * num_4);
+    //    println!("{num_3} / {num_4} = {}", num_3 / num_4);
+    //   println!("{num_3} % {num_4} = {}", num_3 % num_4);
+}
+fn numbers() {
+    // Unsigned integer: u8, u16, u32, u64, u128, usize
+    // Signed integer: i8, i16, i32, i64, i128, isize
+    println!("Max u32: {}", u32::MAX);
+    println!("Max u64: {}", u64::MAX);
+    println!("Max u128: {}", u128::MAX);
+    println!("Max usize: {}", usize::MAX);
+    println!("Max f32: {}", f32::MAX);
+    println!("Max f64: {}", f64::MAX);
+}
+fn booleans() {
+    let is_true = true;
+    print!("{}", is_true);
+}
+fn strings() {
     println!("Hello, world!"); // println! is from std::macros meaning is a call to the native C runtime std library
     let mut input = String::new(); // A UTF-8–encoded, growable string
     let un_mutable_string = "New String"; // String with double quote
